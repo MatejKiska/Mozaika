10249a048751c8863e5c48280d001d387dfc3b01
MatejKiska <75702645+MatejKiska@users.noreply.github.com>
2020-12-16 16:00:33 +0100

    Create spracovanie_obrazka.m

---
+++ spracovanie_obrazka.m
@@ -0,0 +1,32 @@
+clc, clear
+
+input = imread('input.jpg');
+
+
+img_resized_rgb = imresize(input,[80 80]);
+
+img_resized_gray = im2gray(img_resized_rgb);
+
+
+
+for i = 1:numel(img_resized_gray)
+    if img_resized_gray(i) <= 26
+        img_resized_gray(i) = 0;
+    elseif img_resized_gray(i) <= 77 & img_resized_gray(i)>= 27
+            img_resized_gray(i) = 51;
+    elseif img_resized_gray(i) <= 127 & img_resized_gray(i)>= 78
+            img_resized_gray(i) = 102;
+    elseif img_resized_gray(i) <= 178 & img_resized_gray(i)>= 128
+            img_resized_gray(i) = 153;
+    elseif img_resized_gray(i) <= 229 & img_resized_gray(i)>= 179
+            img_resized_gray(i) = 204;
+    elseif img_resized_gray(i) <= 255 & img_resized_gray(i)>= 230
+            img_resized_gray(i) = 255;
+        
+        
+        
+    end
+
+    
+end
+imshow(img_resized_gray)
