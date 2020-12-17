b3ca0a28fbc00f74be58bd4fdfaa71c0f6d1bb15
MatejKiska <75702645+MatejKiska@users.noreply.github.com>
2020-12-17 21:55:48 +0100

    spracovanie obrazka 10x10 plus kocky

--- spracovanie_obrazka.m
+++ spracovanie_obrazka.m
@@ -1,32 +1,11 @@
 clc, clear
 
-input = imread('input.jpg');
+input = imread('input.png');
 
 
-img_resized_rgb = imresize(input,[50 50]);
+img_resized_rgb = imresize(input,[10 10]);
 
-img_resized_gray = im2gray(img_resized_rgb);
+img_resized_vector = im2bw(img_resized_rgb,0.4);
 
 
-
-for i = 1:numel(img_resized_gray)
-    if img_resized_gray(i) <= 26
-        img_resized_gray(i) = 0;
-    elseif img_resized_gray(i) <= 77 & img_resized_gray(i)>= 27
-            img_resized_gray(i) = 51;
-    elseif img_resized_gray(i) <= 127 & img_resized_gray(i)>= 78
-            img_resized_gray(i) = 102;
-    elseif img_resized_gray(i) <= 178 & img_resized_gray(i)>= 128
-            img_resized_gray(i) = 153;
-    elseif img_resized_gray(i) <= 229 & img_resized_gray(i)>= 179
-            img_resized_gray(i) = 204;
-    elseif img_resized_gray(i) <= 255 & img_resized_gray(i)>= 230
-            img_resized_gray(i) = 255;
-        
-        
-        
-    end
-
-    
-end
-imshow(img_resized_gray)
+imshow(img_resized_vector)
