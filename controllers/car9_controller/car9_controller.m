774c4784360c1c76853bdc3b78606a06455f56c4
dominik2905 <75552047+dominik2905@users.noreply.github.com>
2020-12-17 21:53:10 +0100

    controllery

--- controllers/car9_controller/car9_controller.m
+++ controllers/car9_controller/car9_controller.m
@@ -1,5 +1,5 @@
 % MATLAB controller for Webots
-% File:          car1_controller.m
+% File:          car9_controller.m
 % Date:
 % Description:
 % Author:
@@ -15,36 +15,17 @@ TIME_STEP = 64;
 % get and enable devices, e.g.:
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
-motor_left1 = wb_robot_get_device('motor_left1');
-motor_right1 = wb_robot_get_device('motor_right1');
-left_motor_sensor1 = wb_robot_get_device('motor_left1 sensor')
-
-ds1 = wb_robot_get_device('dist_sensor1');
-
-wb_distance_sensor_enable(ds1,TIME_STEP);
-
-wb_position_sensor_enable(left_motor_sensor1,TIME_STEP);
+%  motor = wb_robot_get_device('motor');
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
 while wb_robot_step(TIME_STEP) ~= -1
-  distance1 = wb_distance_sensor_get_value(ds1);
-  disp(distance1);
-  if distance1 < 128
-    wb_motor_set_position(motor_left1,20);
-    wb_motor_set_velocity(motor_left1,2);
-    wb_motor_set_position(motor_right1,20);
-    wb_motor_set_velocity(motor_right1,2);
- 
-    wb_robot_step(10000);
 
-    wb_motor_set_position(motor_left1,0);
-    wb_motor_set_position(motor_right1,0);
-    
-    wb_robot_step(10000);
-  end
+  % read the sensors, e.g.:
+  %  rgb = wb_camera_get_image(camera);
+
   % Process here sensor data, images, etc.
 
   % send actuator commands, e.g.:
